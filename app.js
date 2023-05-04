7418552b496ee6f6b2938921da1cf03a0b1c0677
Abdallah Mustafa <abdallah.ouri@hotmail.com>
2023-05-03 22:49:18 -0500

    initial

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.get("/", function(req, res){
+    console.log("GET Received");
+    res.status(200).sendFile(path.join(__dirname,"index.html"));
+});
+
+app.get("/page1", function(req, res){
+    console.log("GET page1 received");
+    res.status(200).sendFile(path.join(__dirname,"page1.html"));
+});
+
+app.get("/page2", function(req, res){
+    console.log("GET page2 received");
+    res.status(200).sendFile(path.join(__dirname,"page2.html"));
+});
+
+app.post("/", function(req, res){
+    console.log("POST JSON received.");
+    res.status(200).json({"message" : "Hello from JSON"});
+});
+
+const server = app.listen(process.env.PORT, function(){
+    console.log(process.env.MSG_START_SERVER, server.address().port);
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is my solution for assignment#2 - Day2 
