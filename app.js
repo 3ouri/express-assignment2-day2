59c23b2c54624bf8b553c878602d25aaa35255fb
Abdallah Mustafa <abdallah.ouri@hotmail.com>
2023-05-05 10:01:22 -0500

    using express static

--- app.js
+++ app.js
@@ -11,15 +11,17 @@ app.get("/", function(req, res){
     res.status(200).sendFile(path.join(__dirname,"index.html"));
 });
 
-app.get("/page1", function(req, res){
-    console.log("GET page1 received");
-    res.status(200).sendFile(path.join(__dirname,"page1.html"));
-});
+// app.get("/page1", function(req, res){
+//     console.log("GET page1 received");
+//     res.status(200).sendFile(path.join(__dirname,"page1.html"));
+// });
 
-app.get("/page2", function(req, res){
-    console.log("GET page2 received");
-    res.status(200).sendFile(path.join(__dirname,"page2.html"));
-});
+// app.get("/page2", function(req, res){
+//     console.log("GET page2 received");
+//     res.status(200).sendFile(path.join(__dirname,"page2.html"));
+// });
+
+app.use(express.static(path.join(__dirname)));
 
 app.post("/", function(req, res){
     console.log("POST JSON received.");
